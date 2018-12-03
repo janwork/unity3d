4c58495670fa4ea6a376feadf6e26206707e2c8e
janwork <janwork@163.com>
2018-12-03 20:58:58 +0800

    修炼场地图修改

---
+++ darkWonder/Assets/Scripts/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    private CharacterController controller;
+
+    public float speed = 10;
+
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController>();
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        
+       float h = Input.GetAxis("Horizontal");
+       float v = Input.GetAxis("Vertical");
+
+       controller.SimpleMove(new Vector3(h * speed, 0, v * speed));
+	}
+}
