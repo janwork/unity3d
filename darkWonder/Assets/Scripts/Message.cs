c71a20790fa03b21e03826114216a2815007162f
janwork <janwork@163.com>
2018-12-10 18:22:36 +0800

    桥 水面 修炼场

---
+++ darkWonder/Assets/Scripts/Message.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour {
+
+    public static Message _instance;
+    //public GUIText guiText;
+
+	// Use this for initialization
+	void Start () {
+        _instance = this;
+        _instance.gameObject.SetActive(false);
+        print(_instance.gameObject.name);
+	}
+
+    public  static Message getInstance(){
+        return _instance;
+    }
+
+    public void showMessage(string message)
+    {
+        this.gameObject.GetComponent<Text>().text = message;
+        this.gameObject.SetActive(true);
+    }
+
+    public void hideMessage()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ darkWonder/Assets/Scripts/Trainning.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trainning : MonoBehaviour {
+
+    public bool isStart = false;
+    public float timer = 10f;
+    public float resetTimer;
+    public bool isTrainningEnd = false;
+    public GameObject player;
+    public GameObject skillOne;
+    public GameObject message;
+
+    void Start()
+    {
+        resetTimer = timer;
+    }
+
+    void Update()
+    {
+        if (isStart && isTrainningEnd == false)
+        {
+            timer -= Time.deltaTime;
+            Message.getInstance().showMessage(" 修炼时间: " + String.Format("{0:F2}", timer));
+            if (timer <= 0)
+            {
+                isTrainningEnd = true;
+                skillOne.SetActive(true);
+                //Message.getInstance().hideMessage();
+                Message.getInstance().showMessage(" 修炼完成");
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == player.name && isTrainningEnd == false)
+        {
+            isStart = true;
+            timer = resetTimer;
+            message.SetActive(true);
+        }       
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == player.name)
+        {
+            isStart = false;
+            Message.getInstance().hideMessage();
+        }
+        
+    }
+}
