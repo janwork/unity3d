6cd02f9e9db1b12e7656a6794c7f833cc1a2c438
janwork <janwork@163.com>
2019-02-13 20:18:11 +0800

    创建UI项目

---
+++ UI/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickStartBtn()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnClickSoundBtn()
+    {
+        Debug.Log("Sound");
+    }
+
+    public void OnClickMailBtn()
+    {
+        Debug.Log("Mail");
+    }
+
+    public void OnClickMoreGameBtn()
+    {
+        Debug.Log("MoreGame");
+    }
+
+    public void OnClickSettingBtn()
+    {
+        Debug.Log("Setting");
+    }
+
+}
