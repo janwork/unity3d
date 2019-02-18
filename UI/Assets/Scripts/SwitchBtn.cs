a9f0579ed9c103c3892da67c3c68810543b2b5f4
janwork <janwork@163.com>
2019-02-18 12:10:52 +0800

    UI 界面Done，登录，设置，背包，主界面，选择关卡，任务，游戏界面

---
+++ UI/Assets/Scripts/SwitchBtn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchBtn : MonoBehaviour {
+
+    public GameObject switchOn;
+    public GameObject switchOff;
+    private Toggle toggle;
+
+	// Use this for initialization
+	void Start () {
+        toggle = this.GetComponent<Toggle>();
+        if (toggle.isOn)
+        {
+            switchOn.SetActive(true);
+            switchOff.SetActive(false);
+        }
+        else
+        {
+            switchOn.SetActive(false);
+            switchOff.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+    public void OnValueChanged(bool isOn)
+    {
+        switchOn.SetActive(isOn);
+        switchOff.SetActive(!isOn);
+    }
+}
